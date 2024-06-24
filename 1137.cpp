e507aee31be687b8145e39784e80d026f5a3d6de
Sergey <79244748+Hambar17umyan@users.noreply.github.com>
2024-06-24 10:16:29 +0400

    Create 1137.cpp

---
+++ 1137.cpp
@@ -0,0 +1,213 @@
+#include <iostream>
+#include <algorithm>
+#include <math.h>
+#include <sstream>
+#include <string>
+#include <iomanip>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <set>
+#include <map>
+#include <vector>
+#include <iterator>
+using namespace std;
+ 
+//defines
+#define ll long long
+#define usg unsigned
+#define kap map
+#define print(x, n) for(int for_loop = 0; for_loop < n; for_loop++){cout<<x[for_loop]<<' ';}cout<<endl; 
+#define read(x, n) for(int for_loop = 0; for_loop < n; for_loop++){cin>>x[for_loop];} 
+#define speed ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define ratdig(x) cout << fixed << setprecision(x);
+#define xfixdig(x) cout << setprecision(x);
+#define multi int t; cin>>t; presolve(); while(t--) solve()
+#define single presolve(); solve(); return 0
+#define rev(x) reverse(x.begin(), x.end())
+#define all(x) x.begin(), x.end()
+ 
+//functions
+void yn(bool b)
+{
+	if (b)
+	{
+		cout << "YES\n";
+		return;
+	}
+	cout << "NO\n";
+}
+ 
+ll gcd(ll a, ll b) {
+	if (a == 0)
+		return b;
+	if (b == 0)
+		return a;
+	return gcd(b % a, a);
+}
+ 
+ll lcm(ll a, ll b)
+{
+	return (a * b) / gcd(a, b);
+}
+ 
+string to2(ll a)
+{
+	string r = "";
+	for (ll i = a; i > 0; )
+	{
+		ll k = i % 2;
+		i /= 2;
+		char c = k + 48;
+		r += c;
+	}
+	if (a == 0)
+	{
+		r = "0";
+	}
+	rev(r);
+ 
+	return r;
+}
+ 
+ll binpow(ll a, ll b, ll mod = -1)
+{
+	ll ans = 1;
+	while (b)
+	{
+		if ((b & 1) == 1)
+		{
+			ans *= a;
+			if (mod != -1)
+				ans %= mod;
+		}
+		b >>= 1;
+		a *= a;
+		if (mod != -1)
+			a %= mod;
+	}
+	return ans;
+}
+ 
+//body
+ 
+void presolve()
+{
+ 
+}
+const int N = 200005;
+vector<ll>g[N];
+ll mp[N];
+ll arr[N];
+ll tin[N], tout[N];
+ll timer = -1;
+ll t[N * 4], prr[N];
+#define poss pos * 2
+#define midd mid + 1
+#define middle ll mid = (l + r) / 2
+ 
+void build(ll l, ll r, ll pos)
+{
+	middle;
+	if (l == r)
+	{
+		t[pos] = arr[r];
+		prr[l] = pos;
+		return;
+	}
+	build(l, mid, poss);
+	build(midd, r, poss + 1);
+	t[pos] = t[poss] + t[poss + 1];
+}
+ 
+ll query(ll l, ll r, ll ql, ll qr, ll pos)
+{
+	if (l == ql && r == qr)
+	{
+		return t[pos];
+	}
+	middle;
+	if (qr < midd)
+	{
+		return query(l, mid, ql, qr, poss);
+	}
+	if (mid < ql)
+		return query(midd, r, ql, qr, poss + 1);
+	return query(l, mid, ql, mid, poss) + query(midd, r, midd, qr, poss + 1);
+}
+ 
+void update(ll pos, ll x)
+{
+	pos = prr[tin[pos]];
+	t[pos] = x;
+	pos /= 2;
+	while (pos)
+	{
+		t[pos] = t[poss] + t[poss + 1];
+		pos /= 2;
+	}
+}
+ 
+void dfs(ll gag, ll prev = 0)
+{
+	arr[timer + 1] = mp[gag];
+	tin[gag] = ++timer;
+	for (ll i = 0; i < g[gag].size(); i++)
+	{
+		if (g[gag][i] == prev)
+			continue;
+		dfs(g[gag][i], gag);
+	}
+	tout[gag] = timer;
+}
+ 
+void solve()
+{
+	ll x, i, j, n, m, k;
+	cin >> n >> m;
+	for (i = 0; i < n; i++)
+		cin >> mp[i];
+	for (i = 1; i < n; i++)
+	{
+		cin >> j >> k;
+		k--;
+		j--;
+		g[k].push_back(j);
+		g[j].push_back(k);
+	}
+	dfs(0);
+	short op;
+	build(0, n - 1, 1);
+	//cout << "TIMER " << timer << endl;
+	/*for (i = 0; i < n; i++)
+	{
+		cout << arr[i] << ' ';
+	}
+	cout << endl;*/
+	while (m--)
+	{
+		cin >> op;
+		if (op == 1)
+		{
+			cin >> i >> j;
+			i--;
+			update(i, j);
+		}
+		else
+		{
+			cin >> i;
+			i--;
+			cout << query(0, n - 1, tin[i], tout[i], 1) << endl;
+		}
+ 
+	}
+}
+ 
+int main()
+{
+	speed;
+	single;
+	multi;
+ 
+	return 0;
+}
