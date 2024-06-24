34edca25b4fd854d95bfc2e207f5083edac517b0
Sergey <79244748+Hambar17umyan@users.noreply.github.com>
2024-06-24 10:18:11 +0400

    Create 1688.cpp

---
+++ 1688.cpp
@@ -0,0 +1,170 @@
+#include <iostream>
+#include <algorithm>
+#include <math.h>
+#include <sstream>
+#include <string>
+#include <iomanip>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <set>
+#include <map>
+#include <vector>
+#include <iterator>
+using namespace std;
+ 
+//defines
+#define ll long long
+#define usg unsigned
+#define kap map
+#define print(x, n) for(int for_loop = 0; for_loop < n; for_loop++){cout<<x[for_loop]<<' ';}cout<<endl; 
+#define read(x, n) for(int for_loop = 0; for_loop < n; for_loop++){cin>>x[for_loop];} 
+#define speed ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define ratdig(x) cout << fixed << setprecision(x);
+#define xfixdig(x) cout << setprecision(x);
+#define multi int t; cin>>t; presolve(); while(t--) solve()
+#define single presolve(); solve(); return 0
+#define rev(x) reverse(x.begin(), x.end())
+#define all(x) x.begin(), x.end()
+ 
+//functions
+void yn(bool b)
+{
+	if (b)
+	{
+		cout << "YES\n";
+		return;
+	}
+	cout << "NO\n";
+}
+ 
+ll gcd(ll a, ll b) {
+	if (a == 0)
+		return b;
+	if (b == 0)
+		return a;
+	return gcd(b % a, a);
+}
+ 
+ll lcm(ll a, ll b)
+{
+	return (a * b) / gcd(a, b);
+}
+ 
+string to2(ll a)
+{
+	string r = "";
+	for (ll i = a; i > 0; )
+	{
+		ll k = i % 2;
+		i /= 2;
+		char c = k + 48;
+		r += c;
+	}
+	if (a == 0)
+	{
+		r = "0";
+	}
+	rev(r);
+ 
+	return r;
+}
+ 
+ll binpow(ll a, ll b, ll mod = -1)
+{
+	ll ans = 1;
+	while (b)
+	{
+		if ((b & 1) == 1)
+		{
+			ans *= a;
+			if (mod != -1)
+				ans %= mod;
+		}
+		b >>= 1;
+		a *= a;
+		if (mod != -1)
+			a %= mod;
+	}
+	return ans;
+}
+ 
+//body
+ 
+void presolve()
+{
+ 
+}
+ 
+ll l, timer = 0;
+ll par[200005], up[200005][32];
+vector<ll>child[200005];
+ll tin[200005], tout[200005];
+ 
+void dfs(ll gag)
+{
+	tin[gag] = ++timer;
+	up[gag][0] = par[gag];
+	for (ll i = 1; i <= l; i++)
+	{
+		up[gag][i] = up[up[gag][i - 1]][i - 1];
+	}
+	for (ll i = 0; i < child[gag].size(); i++)
+	{
+		dfs(child[gag][i]);
+	}
+ 
+	tout[gag] = ++timer;
+}
+ 
+bool isupper(ll a, ll b)
+{
+	return tin[a] <= tin[b] && tout[b] <= tout[a];
+}
+ 
+ll lca(ll a, ll b)
+{
+	if (isupper(a, b)) return a;
+	if (isupper(b, a)) return b;
+	for (ll i = l; i >= 0; i--)
+	{
+		if (!isupper(up[a][i], b))
+		{
+			a = up[a][i];
+		}
+	}
+	return up[a][0];
+}
+ 
+void solve()
+{
+	ll x, i, j, n, m, k;
+	cin >> n >> m;
+	timer = 0;
+	for (i = 1; i < n; i++)
+	{
+		cin >> j;
+		j--;
+		par[i] = j;
+		child[j].push_back(i);
+	}
+	l = 1;
+	while ((1 << l) <= n)  l++;
+	dfs(0);
+	while (m--)
+	{
+		cin >> i >> j;
+		i--;
+		j--;
+		cout << lca(i, j) + 1 << endl;
+	}
+}
+ 
+int main()
+{
+	speed;
+	single;
+	multi;
+ 
+	return 0;
+}
