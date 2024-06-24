0fa3b9d61e9bb2c5b571d4a5246c46da8c741efb
Sergey <79244748+Hambar17umyan@users.noreply.github.com>
2024-06-24 10:41:27 +0400

    Create 1074.cpp

---
+++ 1074.cpp
@@ -0,0 +1,133 @@
+#include <iostream>
+#include <algorithm>
+#include <math.h>
+#include <sstream>
+#include <string>
+#include <iomanip>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <set>
+#include <map>
+#include <vector>
+#include <iterator>
+using namespace std;
+ 
+//defines
+#define ll long long
+#define usg unsigned
+#define kap map
+#define print(x, n) for(int for_loop = 0; for_loop < n; for_loop++){cout<<x[for_loop]<<' ';}cout<<endl; 
+#define read(x, n) for(int for_loop = 0; for_loop < n; for_loop++){cin>>x[for_loop];} 
+#define speed ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define ratdig(x) cout << fixed << setprecision(x);
+#define xfixdig(x) cout << setprecision(x);
+#define multi int t; cin>>t; presolve(); while(t--) solve()
+#define single presolve(); solve(); return 0
+#define rev(x) reverse(x.begin(), x.end())
+#define all(x) x.begin(), x.end()
+ 
+//functions
+void yn(bool b)
+{
+	if (b)
+	{
+		cout << "YES\n";
+		return;
+	}
+	cout << "NO\n";
+}
+ 
+ll gcd(ll a, ll b) {
+	if (a == 0)
+		return b;
+	if (b == 0)
+		return a;
+	return gcd(b % a, a);
+}
+ 
+ll lcm(ll a, ll b)
+{
+	return (a * b) / gcd(a, b);
+}
+ 
+string to2(ll a)
+{
+	string r = "";
+	for (ll i = a; i > 0; )
+	{
+		ll k = i % 2;
+		i /= 2;
+		char c = k + 48;
+		r += c;
+	}
+	if (a == 0)
+	{
+		r = "0";
+	}
+	rev(r);
+ 
+	return r;
+}
+ 
+ll binpow(ll a, ll b, ll mod = -1)
+{
+	ll ans = 1;
+	while (b)
+	{
+		if ((b & 1) == 1)
+		{
+			ans *= a;
+			if (mod != -1)
+				ans %= mod;
+		}
+		b >>= 1;
+		a *= a;
+		if (mod != -1)
+			a %= mod;
+	}
+	return ans;
+}
+ 
+//body
+ 
+void presolve()
+{
+ 
+}
+const int N = 200005;
+ll pr[N];
+void solve()
+{
+	ll x, i, j, n, m, k;
+	cin >> n;
+	set<ll>st;
+	ll sum = 0;
+	for (i = 0; i < n; i++) 
+	{ 
+		cin >> pr[i];
+		sum += pr[i];
+	}
+	k = 0;
+	m = 0;
+	sort(pr, pr + n);
+	ll res = sum - pr[0] * n;
+	k = pr[0];
+	for (i = 1; i < n; i++)
+	{
+		m = sum - k - pr[i] * (n - i);
+		m += pr[i] * i - k;
+		res = min(res, m);
+		k += pr[i];
+	}
+	cout << res << endl;
+}
+ 
+int main()
+{
+	speed;
+	single;
+	multi;
+ 
+	return 0;
+}
